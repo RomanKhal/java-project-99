425a3c6e08be5239c770904945db9b04da83df73
roman <kharomatic@gmail.ru>
2024-07-31 14:35:12 +0300

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
 	java
+	jacoco
+	id("io.freefair.lombok") version "8.6"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
 }
