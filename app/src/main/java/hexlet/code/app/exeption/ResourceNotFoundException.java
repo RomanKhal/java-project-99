61e59f823a18880a0bfa38dedab3ce7f2cfe2fce
roman <kharomatic@gmail.ru>
2024-08-09 16:11:50 +0300

    3

--- app/src/main/java/hexlet/code/app/exeption/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hexlet.code.app.exeption;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- app/src/main/java/hexlet/code/app/exeption/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package hexlet.code.app.exeption;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException() {
-        super("User already exists.");
-    }
-}
